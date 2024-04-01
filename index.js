150807e8d707b868ba27d55672d47243f993d5a3
kiranazka <160616395+kiranazka@users.noreply.github.com>
2024-04-01 13:31:23 -0700

    Simple Calculator
    My first project

---
+++ index.js
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+let answers = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "firstNumber" },
+    { message: "Enter Second Number", type: "number", name: "secondNumber" },
+    {
+        messag: "select one operator to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["+", "-", "/", "*"],
+    }
+]);
+// console.log(answers);
+// condition operator if else
+if (answers.operator === "+") {
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if (answers.operator === "-") {
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if (answers.operator === "/") {
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else if (answers.operator === "*") {
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if (answers.operator === "**") {
+    console.log(answers.firstNumber ** answers.secondNumber);
+}
